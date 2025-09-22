cf64141fdcc9f8aeaef3eeaa38892515122e418e
UnaRamita <zelmarrodriguez97@gmail.com>
2025-09-22 17:05:44 -0300

    Cambios en el comportamiento de los enemigos
    Estoy intentando basicamente implementar un movimiento en blocke en los enemigos. cambios en el behavior y en la clase de los enemigos. (la idea que tengo del behavior es que le envie a cada enemigo la orden de lo que tiene que hacer y que en los enemigos solo tengan el borrarse, actualizar su posicion (respecto a lo que les mande el behavior) y dibujarse en pantalla

--- Tp Final Introduccion a La Programacion .cpp
+++ Tp Final Introduccion a La Programacion .cpp
@@ -9,20 +9,20 @@ using namespace std;
 const int bordeSup = 3;
 const int bordeIzq = 3;
 const int bordeDer = 43;
-const int bordeInf = 30;
-//la idea de este bool es que cuando un enemigo toque el borde cambien todos de direccion
-bool cMborde=false;
-//a ver si esto soluciona el problema del movimiento en bloque}
-// se movieron el bloque pero no cambiaron de dirrecion no entiendo por que
-int moveX=1;
-int moveY=0;
+const int bordeInf = 29;
+
+
+
+
+clock_t rit;
+clock_t countR;
 
 
 
 //clases principal y subclase de enemigos
 
 class Enemigo{
-protected:
+    protected:
 	int hp = 1; //vida
 	int velocidad;
 	//int moveX;// movimiento en eje x
@@ -31,16 +31,22 @@ protected:
 	int x;
 	int y;
 	char forma;
-	clock_t rit;//tics
-	clock_t countR;//contador de tics
+	/* aca estaban las variables
+	clock_t rit;
+	clock_t countR
+	dsp revisar que no me quede nada de codigo de esto colgado por ahi esto incluye la velocidad que tenian puesta los enemigos
+	*/
 	//los voids los aclaro mas adelante
 	void borrar();
 	void dibujar();
-	void move();
 	void shoot();
+	
 public:
+	
 	Enemigo(int cX,int cY, int vel); // posicion del Enemigo// ese vel dsp sacarlo, es para probar velocidades dsp lo dejo ya puesto en el construct
-	void behavior();
+	void move(int mX, int mY);
+	int getX(){return x;}
+	int getY(){return y;}// por el momento lo agrego de onda, no se si lo voy a usar
 };
 // Enemigos forma color y puntos(puntos no se si lo voy a dejar vere cuando haga el sistema de puntos)
 class EnemigoH : public Enemigo{
@@ -74,13 +80,13 @@ private:
 	int jHp=3;
 	char jForma='A';
 	int color = 2;
-	int moveX;
+	int JmoveX;
 	int moveY;
 	int x,y;
 	//despues probar si al jugador me conviene hacerle voids a parte con otros nombres o es indiferente
 	void borrar();
 	void dibujar();
-	void move();
+	void jmove();
 	void jShoot();
 public:
 	
@@ -90,20 +96,11 @@ public:
 	jugador(int velocidad);
 	
 };
-//	
-//aca van los contrctr (por el momento lo primero va a ser hacer un enemigo y hacerle el comportamiento, despes vere de)
-//
+
 Enemigo::Enemigo(int cX,int cY, int vel){
 	velocidad= vel; //velocidad de prueba, despues probar
 	x = cX;
 	y = cY;
-	
-	
-	//estos 2 chequearlos por que creo q entendi lo que hacen pero no estoy 100 seguro
-	rit= CLOCKS_PER_SEC/velocidad;
-	countR=clock();
-	//
-	
 }
 EnemigoH::EnemigoH(int x,int y) : Enemigo(x,y,1) {
 	forma = 'H';
@@ -130,74 +127,112 @@ textcolor(color);
 cout<< forma;
 }
 
-void Enemigo::move(){
-	
-	if (moveX==1 && moveY ==0){
-		x++;
-		cMborde=false;
-	}
-	else if (moveX==-1 && moveY==0){
-		x--;
-		cMborde=false;
-	}
-	else if(moveY==1 && cMborde==false){
-		y++;
-		moveY=0;
-	}
+void Enemigo::move(int mX, int mY){ //la idea de eso seria en el behavior decirle directamente que se muevan en el eje x o en el y
+	borrar();
+	x = x+mX;
+	y = y+mY;
+	dibujar();
 }
-void Enemigo::behavior(){ // cambiar nombre ?
-	if(rit+countR<clock()){
-		
-		//baja uno
-		if ( bordeDer == x || bordeIzq== x){
-			if(cMborde==false){
-				moveY=1;
-				
-			}
+//Void general, lista de enemigos y n es el numero de enemigos
+void behavior(Enemigo*enemigos[],int n){ // viendo que esto no fuciono como esperaba voy a cambiarlo por un comportamiento centralizado que le diga a todos los enemigos como moverse
+	
+	static int moveX= 1;
+	
+	static bool down = false;
+	static bool edgeD=false;
+	
+	int enemigoR = INT_MIN;
+	int enemigoL = INT_MAX;	
+	
+	//detectar la coordenada x de el enemigo mas a la izq y a la derecha.
+	for (int i=0; i<n; i++){
+		int x= enemigos[i]-> getX();	
+		if (x < enemigoL){
+			enemigoL=x;
 		}
-		else{moveY=0;}
-		//cambio de direccion dsp de hacer lo anterior
-		if (moveX ==1 && cMborde==true){
-			moveX= -1;
+		if (x > enemigoR){
+			enemigoR=x;
 		}
-		else if (moveX ==-1 && cMborde==true){
-			moveX= 1;
-		}
-		
-		
-		borrar();
-		move();
-		dibujar();
-		
-		countR = clock();//importante xd
 	}
+	//cuando itere si tocamos borde en la anterior iteracion baja
+	if(down== true){
+	 
+		for(int i=0; i<n; i++){
+			enemigos[i]-> move(0,1);
+		}
+		moveX = -moveX;//cambia la diereccion del movimiento
+		down = false;
+		edgeD = false;
+		countR = clock();//REINICIA EL CONTADOR, tengo que dejar de olvidarme esto
+		return;
+	}
+
+	//si no estan sobre un borde se mueven en la direccion de moveX
+	if (moveX==1 && down== false){
+		for (int i=0; i<n; i++){
+			enemigos[i]-> move(1,0);
+		}
+	}
+	else if (moveX==-1){
+		for(int i=0;i<n;i++){
+			enemigos[i]-> move(-1,0);
+		}
+	}
+	// si estamos en un borde pone edgeD en true y eso pone down en true;
+	if (enemigoL == bordeIzq){
+		edgeD=true;
+	}
+	else if (enemigoR == bordeDer){
+		edgeD=true;
+	}
+	else {edgeD=false;}
+
+	if (edgeD==true){
+		down=true;
+	}
+	countR = clock();//REINICIA EL CONTADOR, tengo que dejar de olvidarme esto
 }
 
 int main (int argc, char *argv[]) {
 	
+	
+	
 	for(int x = bordeIzq; x <= bordeDer; x++){
-		gotoxy(x,bordeSup);
+		gotoxy(x,bordeSup -1);
 		cout <<"-";
 	}
 	for(int x = bordeIzq; x <= bordeDer; x++){
-		gotoxy(x,bordeInf);
+		gotoxy(x,bordeInf +1);
 		cout <<"-";
 	}
 	for(int y = bordeSup; y <= bordeInf; y++){
-		gotoxy (bordeIzq-1,y);
+		gotoxy (bordeIzq-2,y);
 		cout<<"|";
 	}
 	for(int y = bordeSup; y <= bordeInf; y++){
 		gotoxy (bordeDer+1,y);
 		cout<<"|";
 	}
+	//orden de enemigos W arriba, M medio ,H abajo
+	//x y
+    Enemigo* enemigos[3]; //puebo con 3 uno de cada tipo
+	enemigos[0] = new EnemigoW (4,3);
+	enemigos[1] = new EnemigoM (6,5);
+	enemigos[2] = new EnemigoH (8,7);
 	
-	EnemigoM *em1= new EnemigoM(4,4);
-	EnemigoH *em2= new EnemigoH(4,6);
+	//dibuja en posicion inicial
+	for(int i=0;i<3;i++){
+		enemigos[i]->move(0,0); 
+	}
 	
+	rit = CLOCKS_PER_SEC/3; //uno con lo que vario la velocidad podria poner una variable para cambiarlo pero por ahora va a quedar asi
+	countR = clock();
+
 	while(true){
-		em1 ->behavior();
-		em2 ->behavior();
+		//no se si ponerlo aca, provare
+		if (rit+countR<clock()){
+			behavior(enemigos,3);
+		}
 	}
 	
 	return 0;
